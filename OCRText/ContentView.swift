b9d3c0edb802200291846a07ed62dfb276ce6a80
Nick (Personal) <nick.github1@gmail.com>
2025-07-03 00:25:07 +0800

    Initial Commit

---
+++ OCRText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OCRText
+//
+//  Created by Nick on 2025/7/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OCRText/OCRTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRTextApp.swift
+//  OCRText
+//
+//  Created by Nick on 2025/7/3.
+//
+
+import SwiftUI
+
+@main
+struct OCRTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
